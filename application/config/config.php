23dfc345512c8a87f88cb2d1fc16ff9c676aa51a
Von Matthew Alfafara <valfafara@yondu.com>
2019-03-08 18:40:21 +0800

    s

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-// $config['base_url'] = 'http://localhost/ticket/';
+$config['base_url'] = 'http://localhost/ticket/';
 // $config['base_url'] = 'https://alfa-ticketing-app-v01.herokuapp.com/';
 
 /*
